5e1c5b71068bc348fddea75cffbafdd1721e9524
inbary-s <inbarsim6@gmail.com>
2020-03-28 09:36:16 -0600

    build

---
+++ build/precache-manifest.e571a2e02a6d7e8aa69e7066cd644a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ee8a1b0a56539de6111bc822b164e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df40fe0d23386048c140",
+    "url": "/static/css/main.5758e9d4.chunk.css"
+  },
+  {
+    "revision": "c9a7ac3e9dd2d65c0ed5",
+    "url": "/static/js/2.085ea49f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.085ea49f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df40fe0d23386048c140",
+    "url": "/static/js/main.c7080901.chunk.js"
+  },
+  {
+    "revision": "3ecf410d7eb275d95835",
+    "url": "/static/js/runtime-main.a90ecbee.js"
+  }
+]);
